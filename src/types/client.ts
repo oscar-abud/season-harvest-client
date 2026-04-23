3d11a67267d54ea4737750d2eb3568c667670222
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-04-23 17:18:00 -0400

    [FEAT] add type of client and cart

---
+++ src/types/client.ts
@@ -0,0 +1,21 @@
+export interface ICart {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  priceTotal: number;
+}
+
+export interface IClient {
+  _id: string;
+  name: string;
+  lastname: string;
+  email: string;
+  password: string;
+  birthdate: string;
+  authProvider: string;
+  carts: ICart[];
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
